bec1980885bf5517d3c3f6cd67f47df824170af9
AnushangaWimalasena <anushangawimalasena95@gmail.com>
2021-01-08 06:06:54 +0530

    add colab config

--- human_recognize/config.py
+++ human_recognize/config.py
@@ -1,10 +1,10 @@
 # YOLO options
 YOLO_TYPE                   = "yolov3" # yolov4 or yolov3
 YOLO_FRAMEWORK              = "tf" # "tf" or "trt"
-YOLO_V3_WEIGHTS             = "../model_data/yolov3.weights"
-YOLO_V4_WEIGHTS             = "../model_data/yolov4.weights"
-YOLO_V3_TINY_WEIGHTS        = "../model_data/yolov3-tiny.weights"
-YOLO_V4_TINY_WEIGHTS        = "../model_data/yolov4-tiny.weights"
+YOLO_V3_WEIGHTS             = "/content/drive/MyDrive/FYP/yolov3.weights"
+YOLO_V4_WEIGHTS             = "/content/drive/MyDrive/FYP/yolov3.weights"
+YOLO_V3_TINY_WEIGHTS        = "/content/drive/MyDrive/FYP/yolov3.weights"
+YOLO_V4_TINY_WEIGHTS        = "/content/drive/MyDrive/FYP/yolov3.weights"
 YOLO_TRT_QUANTIZE_MODE      = "INT8" # INT8, FP16, FP32
 YOLO_CUSTOM_WEIGHTS         = False # "checkpoints/yolov3_custom" # used in evaluate_mAP.py and custom model detection, if not using leave False
                             # YOLO_CUSTOM_WEIGHTS also used with TensorRT and custom model detection
@@ -26,8 +26,8 @@ if YOLO_TYPE                == "yolov3":
 TRAIN_YOLO_TINY             = False
 TRAIN_SAVE_BEST_ONLY        = True # saves only best model according validation loss (True recommended)
 TRAIN_SAVE_CHECKPOINT       = False # saves all best validated checkpoints in training process (may require a lot disk space) (False recommended)
-TRAIN_CLASSES               = "../mnist/mnist.names"
-TRAIN_ANNOT_PATH            = "../mnist/mnist_train.txt"
+TRAIN_CLASSES               = "/content/drive/MyDrive/FYP/YOLO/mnist/mnist.names"
+TRAIN_ANNOT_PATH            = "/content/drive/MyDrive/FYP/YOLO/mnist/mnist_train.txt"
 TRAIN_LOGDIR                = "log"
 TRAIN_CHECKPOINTS_FOLDER    = "checkpoints"
 TRAIN_MODEL_NAME            = f"{YOLO_TYPE}_custom"
@@ -43,7 +43,7 @@ TRAIN_WARMUP_EPOCHS         = 2
 TRAIN_EPOCHS                = 100
 
 # TEST options
-TEST_ANNOT_PATH             = "../mnist/mnist_test.txt"
+TEST_ANNOT_PATH             = "/content/drive/MyDrive/FYP/YOLO/mnist/mnist_test.txt"
 TEST_BATCH_SIZE             = 4
 TEST_INPUT_SIZE             = 416
 TEST_DATA_AUG               = False
